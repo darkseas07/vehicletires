795e0acf058afa10bf1273d19f163806bec1cf38
Darkseas <darkseas07@gmail.com>
2021-02-03 02:15:37 +0300

    Add files via upload

---
+++ vehicletires/client/main.lua
@@ -0,0 +1,21 @@
+local isModified = false
+local topMPH = 30.0
+
+Citizen.CreateThread(function()
+ 	while true do
+ 		Citizen.Wait(1000)
+		local ped = PlayerPedId()
+		if IsPedInAnyVehicle(ped, false) then
+			local veh = GetVehiclePedIsUsing(ped)
+			local vehWheelCount = GetVehicleNumberOfWheels(veh)
+			for i = 0, vehWheelCount + 1 do
+				if IsVehicleTyreBurst(veh, i, 0) and GetPedInVehicleSeat(veh, -1) == ped and not isModified then
+					isModified = true
+					SetVehicleMaxSpeed(veh, topMPH)
+				end
+			end
+		else
+			isModified = false
+		end
+ 	end
+ end)
